ac7081a4c41bb89a7f6dcd90f33cfe401376d606
Seb <sebastien.nuss@gmail.com>
2014-09-12 21:52:04 +0200

    adds test to bound variable for roll
    method roll tests for argument type and values set between [0-10].

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -12,5 +12,9 @@ class BowlingGame
 
     public function roll($pins)
     {
+    	if ( !is_int ($pins) )
+		throw new \InvalidArgumentException ('ERROR : roll need an integer between 0 and , '$pins' provided.'); 
+	if ( ($pins < 0) || ($pins > 10) )
+		throw new \OutOfRangeException 'roll needs an integer between 0 and 10'); 
     }
 }
