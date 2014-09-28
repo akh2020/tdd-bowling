33a8736a607406b8d2c00b59799ecb9d1972856a
akh2020 <p@s.ici>
2014-09-28 13:31:44 +0200

    add test for a 2 strike game

--- tests/unit/Team2020/BowlingGameTest.php
+++ tests/unit/Team2020/BowlingGameTest.php
@@ -96,4 +96,37 @@ class BowlingGameTest extends \Codeception\TestCase\Test
         $this->game->roll(9);
         $this->assertEquals(28, $this->game->score(), 'after a spare (4,6) and a roll of 9, the score must be 28');
     }
+
+    public function testTwoStrikesGame()
+    {
+        $this->game->roll(1);
+        $this->game->roll(2);
+        $this->assertEquals(3, $this->game->score(), 'after 1st frame (1,2), the score must be 3');
+        $this->game->roll(10);
+        $this->assertEquals(13, $this->game->score(), 'after 2nd frame (X), the score must be 13');
+        $this->game->roll(10);
+        $this->assertEquals(33, $this->game->score(), 'after 3nd frame (X), the score must be 33');
+        $this->game->roll(4);
+        $this->game->roll(3);
+        $this->assertEquals(51, $this->game->score(), 'after 4th frame (4,3), the score must be 51');
+        $this->game->roll(7);
+        $this->game->roll(3);
+        $this->assertEquals(61, $this->game->score(), 'after 5th frame (7,/), the score must be 61');
+        $this->game->roll(9);
+        $this->game->roll(0);
+        $this->assertEquals(79, $this->game->score(), 'after 6th frame (9,0), the score must be 79');
+        $this->game->roll(1);
+        $this->game->roll(7);
+        $this->assertEquals(87, $this->game->score(), 'after 7th frame (1,7), the score must be 87');
+        $this->game->roll(2);
+        $this->game->roll(6);
+        $this->assertEquals(95, $this->game->score(), 'after 8th frame (2,6), the score must be 95');
+        $this->game->roll(5);
+        $this->game->roll(5);
+        $this->assertEquals(105, $this->game->score(), 'after 9th frame (5,/), the score must be 105');
+        $this->game->roll(0);
+        $this->game->roll(6);
+        $this->assertEquals(111, $this->game->score(), 'after the last frame (0,6), the score must be 111');
+    }
+
 }
